612bf204e46cb37c456be5a940c7f5c724fd613d
Richer Yang <fantasyworldidvtw@gmail.com>
2026-07-25 14:25:59 +0800

    修正測試模式設定值覆蓋問題

--- admin/Page/html/option.php
+++ admin/Page/html/option.php
@@ -5,7 +5,7 @@ use RY\Invoice\Ecpay\LinkProvider;
 
 ?>
 
-<?php $api_info = LinkProvider::instance()->get_api_info(); ?>
+<?php $api_info = LinkProvider::instance()->get_api_info(false); ?>
 
 <h2 class="title"><?php esc_html_e('API credentials', 'ry-invoice-for-ecpay'); ?></h2>
 
