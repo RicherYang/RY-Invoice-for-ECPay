ef26100a72db7e45fcf8632ef74762b2b2ce0df4
Richer Yang <fantasyworldidvtw@gmail.com>
2026-07-30 09:47:38 +0800

    調整文字過濾內容

--- classes/LinkProvider.php
+++ classes/LinkProvider.php
@@ -95,8 +95,8 @@ final class LinkProvider
                 $invoice_item['qty'] = 1;
             }
 
-            $name = mb_strimwidth(str_replace('|', '', wp_strip_all_tags($invoice_item['name'])), 0, 80, '');
-            $unit = mb_strimwidth(str_replace('|', '', wp_strip_all_tags($invoice_item['unit'])), 0, 6, '');
+            $name = mb_strimwidth($this->clean_string($invoice_item['name']), 0, 80, '');
+            $unit = mb_strimwidth($this->clean_string($invoice_item['unit']), 0, 6, '');
             $qty = round($invoice_item['qty'], $general_info['count_precision']);
             $total = $invoice_item['total'];
             if ($post_args['vat'] === 0) {
@@ -129,7 +129,7 @@ final class LinkProvider
         $post_args['TaxAmount'] = $post_args['SalesAmount'] - $salse_amount;
 
         $post_args['InvoiceRemark'] = apply_filters('ry_invoice-main_remark', $post_args['InvoiceRemark'], $object_ID);
-        $post_args['InvoiceRemark'] = mb_strimwidth(wp_strip_all_tags($post_args['InvoiceRemark']), 0, 200, '');
+        $post_args['InvoiceRemark'] = mb_strimwidth($this->clean_string($post_args['InvoiceRemark']), 0, 200, '');
 
         foreach ($post_args as $key => $value) {
             if (is_array($value)) {
@@ -231,6 +231,13 @@ final class LinkProvider
         $trade_no = apply_filters('ry_invoice_ecpay-trade_no', $trade_no, $object_ID, $order_prefix);
 
         return substr($trade_no, 0, 18);
+    }
+
+    protected function clean_string(string $string)
+    {
+        $string = wp_strip_all_tags($string);
+        $string = trim(str_replace(["\r", "\n", "\t"], '', $string));
+        return str_replace(['|', '<', '>', '&', ':', '\'', '"', '`'], '', $string);
     }
 
     protected function link_server(string $url, array $args, string $MerchantID, string $HashKey, string $HashIV, int $timeout = 30)
